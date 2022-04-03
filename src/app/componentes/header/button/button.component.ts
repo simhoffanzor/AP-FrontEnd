98ebadc731f9bb6b214a7ce5a1a4b11acc5d47d6
simhoffanzor <simhoffanzor@gmail.com>
2022-04-03 18:15:12 -0300

    agrego Input al botón

--- src/app/componentes/header/button/button.component.ts
+++ src/app/componentes/header/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
-  btntext : string = "Login";
+  @Input() btntext : string = "";
 
   constructor() { }
 
